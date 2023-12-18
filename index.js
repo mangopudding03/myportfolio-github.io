5cd49d7d7cdbe5ef104f4b80c9220372f41886c6
mangopudding03 <100097596+mangopudding03@users.noreply.github.com>
2023-12-18 18:16:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var welcomearray = ["Welcome to my page, my name is Maanya"];
+var textPosition = 0;
+var speed = 100;
+
+typewriter = () => {
+    document.querySelector("#welcome").
+    innerHTML = welcomearray[0].substring(0, textPosition)
+
+    if (textPosition++ !=  welcomearray[0].length)
+        setTimeout(typewriter, speed);
+}
+
+window.addEventListener("load", typewriter);
+
