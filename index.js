aa236efbef79c59d1df00dd8f3403a5a965256c7
mangopudding03 <100097596+mangopudding03@users.noreply.github.com>
2024-02-05 23:16:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-var welcomearray = ["Welcome, my name is Maanya"];
+var welcomearray = ['Maanya Naveen'];
+var secondlnarr = ['Computer Science major @ The University of Cincinnati'];
+
+
 var textPosition = 0;
+
 var speed = 100;
 
 typewriter = () => {
     document.querySelector("#welcome").
     innerHTML = welcomearray[0].substring(0, textPosition)
 
+    document.querySelector("#secondlnarr").
+    innerHTML = secondlnarr[0].substring(0, textPosition)
+
     if (textPosition++ !=  welcomearray[0].length)
+        setTimeout(typewriter, speed);
+    
+    else if(textPosition++ !=  secondlnarr[0].length)
         setTimeout(typewriter, speed);
 }
 
-window.addEventListener("load", typewriter);
 
+
+window.addEventListener("load", typewriter);
